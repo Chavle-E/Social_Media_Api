a23e60555c8d78fd087b7a93a3b9974dca69b50d
Chavle-E <chavleshviliguka@gmail.com>
2024-03-28 00:56:06 +0400

    Feat:Add single post route and optimize code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
+
 app = FastAPI()
 
 
@@ -8,7 +10,16 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating = Optional[int] = None
+    rating: Optional[int] = None
+
+
+my_posts = []
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
 
 
 @app.get("/")
@@ -18,11 +29,22 @@ async def root():
 
 @app.get("/posts")
 async def get_posts():
-    return {"data": "This are your posts"}
+    return {"data": my_posts}
 
 
-@app.post("/createposts")
-def create_posts(new_post: Post):
-    print(new_post.rating)
-    return {"data": "new post"}
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 10000000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    return {"post_detail": post}
+
+
+
 
